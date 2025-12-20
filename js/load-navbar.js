0691c48e52da318c2cf95ea61e755c8657a905f4
muhajirnasarudin-design.github.io <muhajirnasarudin@gmail.com>
2025-12-20 20:30:04 +0700

    Update load-navbar.js

--- js/load-navbar.js
+++ js/load-navbar.js
@@ -1,21 +1,9 @@
-// Load sidebar ke setiap halaman
-fetch('assets/navbar.html')
-  .then(res => res.text())
-  .then(data => {
-    document.body.insertAdjacentHTML('afterbegin', data);
-
-    const sidebar = document.getElementById("sidebar");
-    const content = document.getElementById("content");
-    const toggle = document.getElementById("toggleSidebar");
-
-    toggle.addEventListener("click", () => {
-      if(sidebar.style.display === "none") {
-        sidebar.style.display = "block";
-        content.style.marginLeft = "210px";
-      } else {
-        sidebar.style.display = "none";
-        content.style.marginLeft = "10px";
-      }
-    });
-  })
-  .catch(err => console.error("Gagal load sidebar:", err));
+document.addEventListener("DOMContentLoaded", function() {
+  const includes = document.querySelectorAll('[data-include]');
+  includes.forEach(el => {
+    fetch(el.getAttribute('data-include'))
+      .then(res => res.text())
+      .then(data => el.innerHTML = data)
+      .catch(err => console.error(err));
+  });
+});
